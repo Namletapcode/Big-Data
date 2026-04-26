4ce2b71254cbfef3310b70a06846cc5f97dd0634
DuySakura <duysakura25@gmail.com>
2026-04-27 00:18:10 +0700

    fix location of state file

--- crawler/src/weather_crawler/weather_fetcher.py
+++ crawler/src/weather_crawler/weather_fetcher.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 logger = logging.getLogger(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATE_FILE = os.path.join(BASE_DIR, 'configs', 'crawler_state.json')
 
 def get_used_idx():
